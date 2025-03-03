32a465197b18d73b10a49dc72fcae0f2f72809aa
Vex048 <borowiak04@gmail.com>
2025-03-03 17:19:41 +0100

    Added a little bit css and added two new views

--- backend/healthcareProject/patients/urls.py
+++ backend/healthcareProject/patients/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     # path('view-records/', views.view_records, name='view_records'),
     path('profile/', views.showPatientProfile, name='profile'),
     path("doctorProfile/<int:id>",doctorProfile,name='doctorProfile'),
+    path('view_records/<int:id>', views.view_records, name='view_records'),
+    path("view_appoinments/<int:id>",views.view_appointments,name='view_appointments'),
 ]
--- backend/healthcareProject/patients/views.py
+++ backend/healthcareProject/patients/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render
 from django.urls import reverse
 
 from doctors.models import Appointment, Doctor, Profile
-from .models import MedicalRecord
+from .models import MedicalRecord, Patient
 # Create your views here.
 def showPatientProfile(request):
     is_doctor = Doctor.objects.filter(user=request.user).exists()
@@ -20,3 +20,17 @@ def showPatientProfile(request):
             appointments = None
         context = {'medical_record': medical_record,"appointments":appointments}
         return render(request, 'patients/profile.html',context)
+
+
+def view_records(request,id):
+    patient = Patient.objects.get(user=request.user)
+    medical_record = MedicalRecord.objects.filter(patient=patient)
+    context = {'medical_record': medical_record}
+    return render(request, 'patients/medicalReport.html',context)
+
+def view_appointments(request,id):
+    patient = Patient.objects.get(user=request.user)
+    appointments = Appointment.objects.filter(patient_id=patient)
+    context = {'appointments': appointments}
+    return render(request, 'patients/appointments.html',context)
+    
